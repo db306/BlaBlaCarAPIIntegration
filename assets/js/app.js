02a602197d166797355c9e5f445447f8be1fdf5d
Daniel Boris <d.boris.iglesia@gmail.com>
2020-08-29 17:03:11 +0200

    feat(front-end): Added Ajax error handling

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,20 +19,37 @@ $( document ).ready( () => {
     function getTrips(){
         $.ajax(window.location + 'search?' + $('form').serialize())
             .done(data => {
-                console.log(data);
-
+                if(data.trips.length === 0){
+                    alert('Woops ❌ No trips were foud');
+                }
                 displayTripOptions(data.trips);
                 updateCursor(data.next_cursor);
 
-            }).fail( err => {
-            console.log('Woops', err);
+            }).fail( (jqXHR,status, err) => {
+                alert('⚠️ status ' + status + ': ' + err + '\n\n' + jqXHR.responseJSON.detail);
         });
     }
 
     function displayTripOptions(trips){
         trips.forEach(trip => {
-            $('.results').append('<div class="trip-entry"><a href="'+trip.link+'"><div>Price: <span class="price">'+ trip.price.amount + ' ' + trip.price.currency +'</span><br>Distance: <span class="distance">' + trip.distance_in_meters + ' Km</span><br>Duration: <span class="time">' + trip.duration_in_seconds + '</span></div></a></div>');
+            $('.results').append('<div class="trip-entry"><a href="'+trip.link+'"><div>Price: <span class="price">'+ trip.price.amount + ' ' + trip.price.currency +'</span><br>Distance: <span class="distance">' + metersToKilometers(trip.distance_in_meters) + ' Km</span><br>Duration: <span class="time">' + formatSeconds(trip.duration_in_seconds) + '</span></div></a></div>');
         });
+    }
+
+    function metersToKilometers(meters){
+        return Math.round(meters / 1000);
+    }
+
+    function formatSeconds(timeInSeconds){
+            const sec_num = parseInt(timeInSeconds, 10);
+            let hours   = Math.floor(sec_num / 3600);
+            let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+            let seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+            if (hours   < 10) {hours   = "0"+hours;}
+            if (minutes < 10) {minutes = "0"+minutes;}
+            if (seconds < 10) {seconds = "0"+seconds;}
+            return hours+':'+minutes+':'+seconds;
     }
 
     function updateCursor(cursor){
